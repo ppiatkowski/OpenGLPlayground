8db887cc4dcd823d94868ef304132aa79fce14d4
Pawel Piatkowski <pawel.piatkowski@mobica.com>
2014-01-02 14:06:59 +0100

    entity : fixed transformation order (scaling must be before rotation)

--- src/entity.cpp
+++ src/entity.cpp
@@ -60,12 +60,13 @@ void Entity::CalculateTransform()
         return;
 
     transform = glm::translate(mat4(1), position);
-    transform = glm::scale(transform, scale);
-
+    
     // TODO do not use Euler angles, use quaternions instead
     transform = glm::rotate(transform, rotation.x, glm::vec3(1.0, 0.0, 0.0));
     transform = glm::rotate(transform, rotation.y, glm::vec3(0.0, 1.0, 0.0));
     transform = glm::rotate(transform, rotation.z, glm::vec3(0.0, 0.0, 1.0));
+
+    transform = glm::scale(transform, scale);
     matrixDirty = false;
 }
 
